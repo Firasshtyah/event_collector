9c0edee207edcb91faf770fb5b0a4880860eb915
firas shtyyah <firas@vogacloset.com>
2023-01-03 14:40:52 +0300

    refactor_V3

--- scripts/deployment_S3.sh
+++ scripts/deployment_S3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if aws s3 ls "s3://vogaeventrecorderstack" 2>&1 | grep -q 'NoSuchBucket'
+if aws s3 ls "s3://voga-event-recorder-stack" 2>&1 | grep -q 'NoSuchBucket'
 then
 aws s3api create-bucket --bucket voga-event-recorder-stack --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
 aws s3api put-public-access-block \

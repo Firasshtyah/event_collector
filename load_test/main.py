b6fcd86b2a5d63fb8a27dc44906f40d1f256d6e4
firas shtyyah <firas@vogacloset.com>
2023-01-04 12:58:14 +0300

    load_test

--- load_test/main.py
+++ load_test/main.py
@@ -11,14 +11,19 @@ def lambda_handler (event, context):
     for i in range(50000):
         response = firehose.put_record(
             DeliveryStreamName="Events_recorder_stream",
-            Record={
-                "session_id" : str(random.randrange(1, 1000000)),
-                "creation_date" : "test",
-                "event_name" : "product_click",
-                "appCodeName" : "test",
-                "appName" : "test" ,
-                "userAgent" : "test",
-                "prodcut_id" : f"product_{str(random.randrange(1, 6))}"
-  
-    }
+                Record={
+                    'Data': json.dumps(
+                    {
+                    "session_id" : str(random.randrange(1, 1000000)),
+                    "creation_date" : "test",
+                    "event_name" : "product_click",
+                    "appCodeName" : "test",
+                    "appName" : "test" ,
+                    "userAgent" : "test",
+                    "prodcut_id" : f"product_{str(random.randrange(1, 6))}"
+                    }
+                        )
+                    
+      
+        }
         )

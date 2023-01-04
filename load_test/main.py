4d929cd6bb372530a791b8086be5ef8e75ce5f94
firas shtyyah <firas@vogacloset.com>
2023-01-04 12:36:47 +0300

    load_test

---
+++ load_test/main.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+import random
+
+firehose = boto3.client('firehose')
+
+def lambda_handler (event, context):
+
+
+    
+    for i in range(50000):
+        response = firehose.put_record(
+            DeliveryStreamName="Events_recorder_stream",
+            Record={
+                "session_id" : str(random.randrange(1, 1000000)),
+                "creation_date" : "test",
+                "event_name" : "product_click",
+                "appCodeName" : "test",
+                "appName" : "test" ,
+                "userAgent" : "test",
+                "prodcut_id" : f"product_{str(random.randrange(1, 6))}"
+  
+    }
+        )
